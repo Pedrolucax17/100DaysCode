09f01407abef1db522c319ea85cac54c6305018e
Pedro Lucas <pl.lucassouza@gmail.com>
2024-03-10 10:03:08 -0300

    Cursor Project Added

---
+++ 18. Cursor/script.js
@@ -0,0 +1,10 @@
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener('mousemove', (e)=>{
+  moveCursor(e.pageX, e.pageY);
+});
+
+const moveCursor = (pageX, pageY) =>{
+  cursor.style.left = pageX + 'px';
+  cursor.style.top = pageY + 'px';
+}
